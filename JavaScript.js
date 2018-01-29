acf18f302596d82fae58e4c7e089d299a32a1b52
Michael Erwin <michaelerwin@michaels-mbp-2.home>
2018-01-29 05:43:47 -0600

    FEF-Final

---
+++ JavaScript.js
@@ -0,0 +1,49 @@
+/*function testing(){
+  let gitHubRequest = new XMLHttpRequest();
+  gitHubRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let gitObject = JSON.parse(this.responseText);
+      document.getElementById("gitcall").innerHTML = gitObject.url;
+    }
+  };
+  gitHubRequest.open("GET", "https://api.github.com/users/mikerwin", true);
+  gitHubRequest.send();
+  }; */
+
+function loadGit(){
+  var xhttp = new XMLHttpRequest();
+
+ xhttp.onreadystatechange = function() {
+     if(this.readyState === 4 && this.status === 200) {
+       createRepos(JSON.parse(this.responseText))
+   }
+ }
+ xhttp.open('GET', 'https://api.github.com/users/mikerwin/repos', true )
+ xhttp.send()
+
+
+function createRepos(repos) {
+    let container = document.getElementById('gitcall');
+ let output = []
+    repos.forEach(function(repo) {
+
+   output.push(`<p>${repo.name} <a href= "${repo.html_url}"></p>`);
+   output.push(`<p>${repo.html_url}<p>`)
+   
+ })
+ container.innerHTML = output.join('');
+}
+}
+
+
+  
+
+  $(document).ready(function(){    
+    $(".icons").hover(function(){
+        $(this).css("font-size", "80px");
+    }, function(){
+        $(this).css("font-size","60px");
+    });
+
+
+});
